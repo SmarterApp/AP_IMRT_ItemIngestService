cf22396f2ae5269a40625346f90db989dd216a3b
swilson11 <sue.wilson4@gmail.com>
2018-04-12 08:15:34 -0700

    Use real endpoint for sync job

--- sync-job.sh
+++ sync-job.sh
@@ -3,6 +3,6 @@ echo Executing Item Sync Job
 
 # Invoke the endpoint to kick off the sync job. The URL is the k8s service name for iis
 # TODO capture the response (jobId) to use for monitoring once that functionality is available
-curl -i "http://ap-imrt-iis-service/systemHook"
+curl -i -X POST "http://ap-imrt-iis-service/sync"
 
 # TODO poll for completion once that functionality is available
