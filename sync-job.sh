c020a7fbb6d385567487c68fa71a1bac5797b7a6
swilson11 <sue.wilson4@gmail.com>
2018-04-13 14:55:49 -0700

    Update item sync to process items in chunks and add ability to stop

--- sync-job.sh
+++ sync-job.sh
@@ -2,7 +2,7 @@
 echo Executing Item Sync Job
 
 # Invoke the endpoint to kick off the sync job. The URL is the k8s service name for iis
-# TODO capture the response (jobId) to use for monitoring once that functionality is available
-curl -i -X POST "http://ap-imrt-iis-service/sync"
+# TODO capture the response (jobId) to use for monitoring
+curl -i -X POST "http://ap-imrt-iis-service/sync/start"
 
-# TODO poll for completion once that functionality is available
+# TODO poll for completion
